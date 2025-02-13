695ce35c1ae99c234051af9a32609f2fbb7f7f9d
Nathan Lam <Nathan.lam.rt@gmail.com>
2025-02-13 12:51:35 +1100

    Don't always follow the brute force solution

--- Codeforces/800/Journey.py
+++ Codeforces/800/Journey.py
@@ -2,7 +2,61 @@
 https://codeforces.com/problemset/problem/2051/B
 """
 
+"""
+Brute force:
+"""
+
+# tests = int(input())
+
+# for _ in range(tests):
+    
+#     distances = str(input()).split(" ")
+#     arr = [int(dist) for dist in distances]
+    
+#     target = arr[0]
+#     travelled = 0 
+    
+#     i = 1
+#     count = 0 
+    
+#     while travelled < target: 
+    
+#         travelled += arr[i]
+#         i += 1 
+#         count += 1
+    
+#         if i >= len(arr):
+#             i = 1 
+    
+#     print(count)
+
+"""
+O(1) Solution: 
+"""
+
 tests = int(input())
+
+for _ in range(tests):
+    
+    
+    distances = [int(dist) for dist in str(input()).split(" ")]
+
+    total = sum(dist for dist in distances)
+    target = distances[0]
+    
+    day3dist = total - target
+    remainder = target % day3dist
+    days = (target // day3dist) * 3 
+    
+    if remainder == 0: 
+        print(days)
+    elif distances[1] >= remainder:
+        print(days + 1)
+    elif distances[1] + distances[2] >= remainder:
+        print(days + 2)
+    else: 
+        print(days + 3)
+
 
 
 
